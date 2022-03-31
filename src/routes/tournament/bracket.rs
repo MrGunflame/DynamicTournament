c3a0ebfb7f797265f107a9677a8f01dd6eefb949
MrGunflame <mrgunflame@protonmail.com>
2022-03-31 03:26:02 +0200

    Fill bracket properly on a bad number of team inputs

--- src/routes/tournament/bracket.rs
+++ src/routes/tournament/bracket.rs
@@ -15,18 +15,56 @@ impl Component for Bracket {
 
     fn create(ctx: &Context<Self>) -> Self {
         let mut initial_round = Vec::with_capacity(ctx.props().tournament.teams.len() / 2);
+        let teams: Vec<Option<api::Team>> = ctx
+            .props()
+            .tournament
+            .teams
+            .clone()
+            .into_iter()
+            .map(|t| Some(t))
+            .collect();
+
+        // Number of matches wanted.
+        let num_wanted = numer_teams_wanted(teams.len());
+
+        // Starting inserting null teams in every uneven index.
+        // let mut i = 1;
+        // while i < num_wanted - teams.len() {
+        //     teams.insert(i, None);
+
+        //     i += 2;
+        // }
+
         let mut i = 0;
-        while i < ctx.props().tournament.teams.len() {
-            let team1 = MatchMember::Entrant(ctx.props().tournament.teams.get(i).unwrap().clone());
-            let team2 = match ctx.props().tournament.teams.get(i + 1) {
-                Some(team) => MatchMember::Entrant(team.clone()),
-                None => MatchMember::Placeholder("null".to_string()),
+        while i < teams.len() {
+            let team1 = MatchMember::Entrant(teams.get(i).unwrap().clone().unwrap());
+            let team2 = match teams.get(i + 1) {
+                Some(Some(team)) => MatchMember::Entrant(team.clone()),
+                _ => MatchMember::Placeholder("null".to_string()),
             };
 
             initial_round.push([team1, team2]);
 
             i += 2;
         }
+
+        let mut i = 0;
+        while initial_round.len() < num_wanted / 2 {
+            let teams = initial_round.get_mut(i).unwrap();
+
+            let fill_match = [
+                teams[1].clone(),
+                MatchMember::Placeholder("null".to_owned()),
+            ];
+
+            teams[1] = MatchMember::Placeholder("null".to_owned());
+
+            initial_round.push(fill_match);
+
+            i += 1;
+        }
+
+        gloo_console::log!(format!("{}", num_wanted));
 
         let mut initial_rounds = vec![initial_round];
         while initial_rounds.last().unwrap().len() > 1 {
@@ -64,6 +102,19 @@ impl Component for Bracket {
                 } else {
                     false
                 }
+
+                // self.state.bracket.update_match(
+                //     round_index,
+                //     match_index,
+                //     match winner_index {
+                //         0 => Winner::Team1,
+                //         1 => Winner::Team2,
+                //         w => panic!("Got winner_index {} in a 2 team match", w),
+                //     },
+                //     |m| {},
+                // );
+
+                // true
             }
         }
     }
@@ -101,6 +152,54 @@ impl Component for Bracket {
             })
             .collect();
 
+        // Starts with the inital number of matches, then halfs it until 1 is reached.
+        // let mut num_matches = ctx.props().tournament.teams.len() / 2;
+        // let mut buffer = 0;
+        // while num_matches > 1 {
+        //     for i in 0..num_matches {
+        //         buffer += 1;
+        //     }
+
+        //     num_matches /= 2;
+        // }
+
+        // let bracket = &self.state.bracket;
+        // let rounds: Html = bracket
+        //     .rounds_iter()
+        //     .enumerate()
+        //     .map(|(round_index, round)| {
+        //         let matches: Html = round
+        //             .iter()
+        //             .enumerate()
+        //             .map(|(match_index, m)| {
+        //                 let on_winner_update =
+        //                     ctx.link()
+        //                         .callback(move |winner_index: usize| Msg::UpdateWinner {
+        //                             round_index,
+        //                             match_index,
+        //                             winner_index,
+        //                         });
+
+        //                 let teams = m.entrants.clone().map(|e| match e {
+        //                     EntrantSpot::Entrant(team) => MatchMember::Entrant(team),
+        //                     EntrantSpot::Empty => MatchMember::Placeholder("null".to_owned()),
+        //                     EntrantSpot::TBD => MatchMember::Placeholder("TBD".to_owned()),
+        //                 });
+
+        //                 html! {
+        //                     <Match teams={teams} on_winner_update={on_winner_update} />
+        //                 }
+        //             })
+        //             .collect();
+
+        //         html! {
+        //             <div class="bracket-round">
+        //                 {matches}
+        //             </div>
+        //         }
+        //     })
+        //     .collect();
+
         html! {
             <div class="bracket-matches">
                 {rounds}
@@ -124,4 +223,13 @@ pub enum Msg {
         match_index: usize,
         winner_index: usize,
     },
+}
+
+fn numer_teams_wanted(teams: usize) -> usize {
+    let mut i = 1;
+    while i < teams {
+        i = i << 1;
+    }
+
+    i
 }
