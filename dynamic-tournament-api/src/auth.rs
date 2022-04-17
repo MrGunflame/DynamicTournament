fdfeb705555508507f153aef3e4d0f9fc86b4605
MrGunflame <mrgunflame@protonmail.com>
2022-04-17 17:04:53 +0200

    Change method for login request to POST

--- dynamic-tournament-api/src/auth.rs
+++ dynamic-tournament-api/src/auth.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 use crate::{Client, Error, Result};
 
 pub struct AuthClient<'a> {
@@ -10,20 +12,26 @@ impl<'a> AuthClient<'a> {
     }
 
     pub async fn login(&self, username: &str, password: &str) -> Result<()> {
-        let auth = format!(
-            "Basic {}",
-            base64::encode(&format!("{}:{}", username, password))
-        );
+        let body = &LoginData {
+            username: username.to_owned(),
+            password: password.to_owned(),
+        };
 
         let req = self
             .client
             .request()
+            .post()
             .url("/v1/auth/login")
-            .header("Authorization", auth.clone());
+            .body(body);
 
         let resp = req.build().send().await?;
 
         if resp.ok() {
+            let auth = format!(
+                "Basic {}",
+                base64::encode(&format!("{}:{}", username, password))
+            );
+
             let mut inner = self.client.inner.write().unwrap();
 
             inner.authorization.update(Some(auth));
@@ -33,4 +41,10 @@ impl<'a> AuthClient<'a> {
             Err(Error::BadStatusCode(resp.status()).into())
         }
     }
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct LoginData {
+    username: String,
+    password: String,
 }
