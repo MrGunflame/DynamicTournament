3e24c6c5be92efc3563fa5b03a15972fed9ec47b
MrGunflame <mrgunflame@protonmail.com>
2022-06-12 03:35:03 +0200

    Reenable /v2/tournament endpoint
    - v2 and v3 use the same data v3 data scheme

--- dynamic-tournament-server/src/http/v2/tournament.rs
+++ dynamic-tournament-server/src/http/v2/tournament.rs
@@ -1,8 +1,17 @@
 use crate::http::{Request, RequestUri};
 use crate::{Error, State, StatusCodeError};
 
-use dynamic_tournament_api::tournament::{BracketType, TournamentId, TournamentOverview};
-use dynamic_tournament_api::v3::id::SystemId;
+use dynamic_tournament_api::tournament::{
+    BracketType, Entrants, Player, Role, Team, Tournament, TournamentId, TournamentOverview,
+};
+use dynamic_tournament_api::v3::id::{RoleId, SystemId};
+use dynamic_tournament_api::v3::tournaments::brackets::Bracket as Bracket2;
+use dynamic_tournament_api::v3::tournaments::entrants::{Entrant, EntrantVariant};
+use dynamic_tournament_api::v3::tournaments::entrants::{Player as Player2, Team as Team2};
+use dynamic_tournament_api::v3::tournaments::{EntrantKind, Tournament as Tournament2};
+use dynamic_tournament_generator::options::TournamentOptionValues;
+use dynamic_tournament_generator::{EntrantScore, SingleElimination};
+use hyper::header::HeaderValue;
 use hyper::{Body, Method, Response, StatusCode};
 
 pub async fn route(
@@ -84,16 +93,148 @@ async fn list(_req: Request, state: State) -> Result<Response<Body>, Error> {
     Ok(resp)
 }
 
-async fn create(_req: Request, _state: State) -> Result<Response<Body>, Error> {
+async fn create(req: Request, state: State) -> Result<Response<Body>, Error> {
+    let body: Tournament = req.json().await?;
+
+    let kind = match body.entrants {
+        Entrants::Players(_) => EntrantKind::Player,
+        Entrants::Teams(_) => EntrantKind::Team,
+    };
+
+    let tournament = Tournament2 {
+        id: 0.into(),
+        name: body.name,
+        description: body.description,
+        date: body.date,
+        kind,
+    };
+
+    let entrants: Vec<Entrant> = match body.entrants {
+        Entrants::Players(players) => players
+            .into_iter()
+            .map(|p| Entrant {
+                id: 0.into(),
+                inner: EntrantVariant::Player(Player2 {
+                    rating: p.rating,
+                    name: p.name,
+                    role: RoleId(0),
+                }),
+            })
+            .collect(),
+        Entrants::Teams(teams) => teams
+            .into_iter()
+            .map(|t| Entrant {
+                id: 0.into(),
+                inner: EntrantVariant::Team(Team2 {
+                    name: t.name,
+                    players: t
+                        .players
+                        .into_iter()
+                        .map(|p| Player2 {
+                            rating: p.rating,
+                            name: p.name,
+                            role: RoleId(0),
+                        })
+                        .collect(),
+                }),
+            })
+            .collect(),
+    };
+
+    let id = state.store.insert_tournament(&tournament).await?;
+
+    let mut entrant_ids = Vec::new();
+    for entrant in entrants {
+        let id = state.store.insert_entrant(id, entrant).await?;
+        entrant_ids.push(id);
+    }
+
+    let bracket = Bracket2 {
+        id: 0.into(),
+        name: tournament.name.clone(),
+        system: match body.bracket_type {
+            BracketType::SingleElimination => SystemId(1),
+            BracketType::DoubleElimination => SystemId(2),
+        },
+        options: match body.bracket_type {
+            BracketType::SingleElimination => TournamentOptionValues::default()
+                .merge(SingleElimination::<u8, EntrantScore<u8>>::options())
+                .unwrap(),
+            BracketType::DoubleElimination => TournamentOptionValues::default(),
+        },
+        entrants: entrant_ids,
+    };
+
+    state.store.insert_bracket(tournament.id, &bracket).await?;
+
     let mut resp = Response::new(Body::empty());
-    *resp.status_mut() = StatusCode::SERVICE_UNAVAILABLE;
+    *resp.status_mut() = StatusCode::CREATED;
+    *resp.body_mut() = Body::from(id.to_string());
 
     Ok(resp)
 }
 
-async fn get(_req: Request, _id: u64, _state: State) -> Result<Response<Body>, Error> {
+async fn get(_req: Request, id: u64, state: State) -> Result<Response<Body>, Error> {
+    let tournament = match state.store.get_tournament(id.into()).await? {
+        Some(t) => t,
+        None => return Err(StatusCodeError::not_found().into()),
+    };
+
+    let entrants = state.store.get_entrants(id.into()).await?;
+
+    let entrants = match entrants.get(0).cloned() {
+        Some(e) => match e.inner {
+            EntrantVariant::Player(_) => Entrants::Players(
+                entrants
+                    .into_iter()
+                    .map(|p| match p.inner {
+                        EntrantVariant::Player(p) => Player {
+                            name: p.name,
+                            role: Role::Unknown,
+                            rating: p.rating,
+                        },
+                        EntrantVariant::Team(_) => unreachable!(),
+                    })
+                    .collect(),
+            ),
+            EntrantVariant::Team(_) => Entrants::Teams(
+                entrants
+                    .into_iter()
+                    .map(|t| match t.inner {
+                        EntrantVariant::Player(_) => unreachable!(),
+                        EntrantVariant::Team(t) => Team {
+                            name: t.name,
+                            players: t
+                                .players
+                                .into_iter()
+                                .map(|p| Player {
+                                    name: p.name,
+                                    rating: p.rating,
+                                    role: Role::Unknown,
+                                })
+                                .collect(),
+                        },
+                    })
+                    .collect(),
+            ),
+        },
+        None => Entrants::Teams(Vec::new()),
+    };
+
+    let body = serde_json::to_vec(&Tournament {
+        id: TournamentId(id),
+        name: tournament.name,
+        description: tournament.description,
+        date: tournament.date,
+        bracket_type: BracketType::SingleElimination,
+        entrants,
+    })?;
+
     let mut resp = Response::new(Body::empty());
-    *resp.status_mut() = StatusCode::SERVICE_UNAVAILABLE;
+    resp.headers_mut()
+        .append("Content-Type", HeaderValue::from_static("application/json"));
+
+    *resp.body_mut() = Body::from(body);
 
     Ok(resp)
 }
