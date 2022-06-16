1568b847dbe63ff3455623acd0c10c5f8328793d
MrGunflame <mrgunflame@protonmail.com>
2022-06-16 22:08:49 +0200

    Add docs for document function

--- dynamic-tournament-web/src/utils/mod.rs
+++ dynamic-tournament-web/src/utils/mod.rs
@@ -3,6 +3,15 @@ mod fetch_data;
 pub use fetch_data::FetchData;
 use web_sys::Document;
 
+/// Returns the root [`Document`].
+///
+/// # Panics
+///
+/// Panics if there is no [`Document`] in root window or no root window is present. This should
+/// never be the case in a web environment.
 pub fn document() -> Document {
-    web_sys::window().unwrap().document().unwrap()
+    web_sys::window()
+        .expect("no window present")
+        .document()
+        .expect("no document present")
 }
