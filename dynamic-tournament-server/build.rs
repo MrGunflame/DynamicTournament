dc4ed9b3324c6638475ed752150255ed53493af3
MrGunflame <mrgunflame@protonmail.com>
2022-06-20 19:44:12 +0200

    Use SECRET_PATH const for all paths

--- dynamic-tournament-server/build.rs
+++ dynamic-tournament-server/build.rs
@@ -4,11 +4,16 @@ use std::process::Command;
 const SECRET_PATH: &str = "jwt-secret";
 
 fn main() {
-    println!("cargo:rerun-if-changed=jwt-secret");
+    println!("cargo:rerun-if-changed={}", SECRET_PATH);
 
     if !Path::new(SECRET_PATH).exists() {
         Command::new("dd")
-            .args(&["if=/dev/urandom", "of=./jwt-secret", "bs=1", "count=512"])
+            .args(&[
+                "if=/dev/urandom",
+                &format!("of={}", SECRET_PATH),
+                "bs=1",
+                "count=512",
+            ])
             .spawn()
             .unwrap();
     }
