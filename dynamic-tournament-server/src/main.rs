ed4c90232740a264bed8daf7090c142e163ae825
MrGunflame <git@robbsrv.de>
2022-08-08 01:42:44 +0200

    cargo fmt

--- dynamic-tournament-server/src/main.rs
+++ dynamic-tournament-server/src/main.rs
@@ -58,53 +58,52 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     {
         let state = state.clone();
-    
-    tokio::task::spawn(async move {
-        let tables = [
-            format!(
-                "CREATE TABLE IF NOT EXISTS {}tournaments (
+
+        tokio::task::spawn(async move {
+            let tables = [
+                format!(
+                    "CREATE TABLE IF NOT EXISTS {}tournaments (
             id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             name TEXT NOT NULL,
             description TEXT NOT NULL,
             date TIMESTAMP NOT NULL,
             kind TINYINT UNSIGNED NOT NULL
         )",
-                prefix
-            ),
-            format!(
-                "CREATE TABLE IF NOT EXISTS {}entrants (
+                    prefix
+                ),
+                format!(
+                    "CREATE TABLE IF NOT EXISTS {}entrants (
             id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             tournament_id BIGINT UNSIGNED NOT NULL,
             data BLOB NOT NULL
         )",
-                prefix
-            ),
-            format!(
-                "CREATE TABLE IF NOT EXISTS {}brackets (
+                    prefix
+                ),
+                format!(
+                    "CREATE TABLE IF NOT EXISTS {}brackets (
             id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             tournament_id BIGINT UNSIGNED NOT NULL,
             data BLOB NOT NULL,
             state BLOB NOT NULL
         )",
-                prefix
-            ),
-            format!(
-                "CREATE TABLE IF NOT EXISTS {}roles (
+                    prefix
+                ),
+                format!(
+                    "CREATE TABLE IF NOT EXISTS {}roles (
             id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             tournament_id BIGINT UNSIGNED NOT NULL,
             name TEXT NOT NULL
         )",
-                prefix
-            ),
-        ];
+                    prefix
+                ),
+            ];
 
-        for t in tables {
-            if let Err(err) = sqlx::query(&t).execute(&state.store.pool).await {
-                log::error!("Failed to create tables: {}", err);
-                break;
+            for t in tables {
+                if let Err(err) = sqlx::query(&t).execute(&state.store.pool).await {
+                    log::error!("Failed to create tables: {}", err);
+                    break;
+                }
             }
-        }
-
         });
     }
 
