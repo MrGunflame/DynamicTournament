7d1b1456e0d427f3c5fb92c4c39876b3dc55151e
MrGunflame <mrgunflame@protonmail.com>
2022-08-18 19:52:55 +0200

    feat: Initial proto definition

--- dynamic-tournament-api/src/v3/tournaments/brackets/matches.rs
+++ dynamic-tournament-api/src/v3/tournaments/brackets/matches.rs
@@ -3,6 +3,82 @@ use dynamic_tournament_core::{EntrantScore, Matches};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Request {
+    compat: u8,
+    command: RequestCommand,
+}
+
+impl Request {
+    pub fn from_bytes(&self) {}
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = Vec::new();
+        buf.push(self.compat);
+        buf.extend(self.command.to_bytes());
+
+        buf
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub enum RequestCommand {
+    /// Reserved for protocol implementations.
+    /// You can safely ignore this when matching the command.
+    Reserved,
+    Authorize(String),
+    /// Synchronize the state of the bracket.
+    SyncState,
+    /// Update the match the given `index` using the data in the
+    /// `nodes`.
+    UpdateMatch {
+        index: u64,
+        nodes: [EntrantScore<u64>; 2],
+    },
+    /// Resets the match at the given `index`.
+    ResetMatch {
+        index: u64,
+    },
+}
+
+impl RequestCommand {
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut buf = Vec::new();
+
+        match self {
+            Self::Reserved => {
+                buf.push(0);
+            }
+            Self::Authorize(token) => {
+                buf.push(1);
+
+                buf.extend(token.len().to_le_bytes());
+                buf.extend(token.as_bytes());
+            }
+            Self::SyncState => {
+                buf.push(2);
+            }
+            Self::UpdateMatch { index, nodes } => {
+                buf.push(3);
+
+                buf.extend(index.to_le_bytes());
+                buf.push(2);
+                buf.extend(nodes[0].score.to_le_bytes());
+                buf.push(nodes[0].winner as u8);
+                buf.extend(nodes[1].score.to_le_bytes());
+                buf.push(nodes[1].winner as u8);
+            }
+            Self::ResetMatch { index } => {
+                buf.push(4);
+
+                buf.extend(index.to_le_bytes());
+            }
+        }
+
+        buf
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Frame {
     Reserved,
     Authorize(String),
