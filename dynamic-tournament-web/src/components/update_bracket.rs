9bcb5d319c35a9741d78356339f43df739ca03f3
MrGunflame <mrgunflame@protonmail.com>
2022-09-03 19:16:10 +0200

    Rework the BracketUpdate.view method and remove all unsafe uses

--- dynamic-tournament-web/src/components/update_bracket.rs
+++ dynamic-tournament-web/src/components/update_bracket.rs
@@ -1,9 +1,7 @@
-use web_sys::HtmlInputElement;
-use yew::prelude::*;
-
-use std::mem::{self, MaybeUninit};
-
 use dynamic_tournament_core::{EntrantScore, EntrantSpot};
+use yew::{html, Callback, Component, Context, Html, Properties};
+
+use crate::components::ParseInput;
 
 pub struct BracketUpdate {
     // Score: [left, right]
@@ -50,75 +48,28 @@ impl Component for BracketUpdate {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let link = ctx.link();
+        let mut entrants: [Html; 2] = [html! {}, html! {}];
+        let mut winners: [Html; 2] = [html! {}, html! {}];
 
-        // SAFETY: inputs is never read from before the value is properly initialized.
-        let mut inputs: [Html; 2] = unsafe { std::mem::zeroed() };
+        for (index, entrant) in entrants.iter_mut().enumerate() {
+            let onchange = ctx
+                .link()
+                .callback(move |value| Msg::UpdateScore(index, value));
 
-        for (i, inp) in inputs.iter_mut().enumerate() {
-            let on_score_update = {
-                let link = link.clone();
-                Callback::from(move |event: InputEvent| {
-                    let input: HtmlInputElement = event.target_unchecked_into();
-                    let value = input.value_as_number() as u64;
+            let score = self.nodes[index].score.to_string();
+            let winner = self.nodes[index].winner;
 
-                    link.send_message(Msg::UpdateScore(i, value))
-                })
-            };
-
-            let value = self.nodes[i].score;
-
-            let team = match ctx.props().teams[i].clone() {
-                EntrantSpot::Entrant(e) => e,
-                // should be unreachable
+            let team = match ctx.props().teams[index].clone() {
+                EntrantSpot::Entrant(entrant) => entrant,
                 _ => "BYE".to_owned(),
             };
 
-            *inp = html! {
-                <div class="popup-team">
-                    <span>{ team }</span>
-                    <br />
-                    <input type="number" value={value.to_string()} oninput={on_score_update} />
-                </div>
-            };
-        }
-
-        // SAFETY: `MaybeUninit` does not require any initialization.
-        let mut teams: [MaybeUninit<Html>; 2] = unsafe { MaybeUninit::uninit().assume_init() };
-
-        let mut winner_input: [MaybeUninit<Html>; 2] =
-            unsafe { MaybeUninit::uninit().assume_init() };
-
-        for ((i, inp), winner_input) in teams.iter_mut().enumerate().zip(winner_input.iter_mut()) {
-            let on_score_update = {
-                let link = link.clone();
-                Callback::from(move |event: InputEvent| {
-                    let input: HtmlInputElement = event.target_unchecked_into();
-                    let value = input.value_as_number() as u64;
-
-                    link.send_message(Msg::UpdateScore(i, value))
-                })
-            };
-
-            let on_winner_update = link.callback(move |_| Msg::UpdateWinner { index: i });
-
-            let value = self.nodes[i].score;
-            let winner = self.nodes[i].winner;
-
-            let team = match ctx.props().teams[i].clone() {
-                EntrantSpot::Entrant(e) => e,
-                // should be unreachable
-                _ => "BYE".to_owned(),
-            };
-
-            inp.write(html! {
-                <tr>
+            *entrant = html! {
+                <tr class="popup-team">
                     <td>{ team.clone() }</td>
-                    <td>
-                        <input class="input-u64" type="number" min="0" value={value.to_string()} oninput={on_score_update}/>
-                    </td>
+                    <td><ParseInput<u64> classes="input-u64" kind="number" value={score} {onchange} /></td>
                 </tr>
-            });
+            };
 
             let classes = if winner {
                 "winner-input active"
@@ -126,16 +77,14 @@ impl Component for BracketUpdate {
                 "winner-input"
             };
 
-            winner_input.write(html! {
-                <button class={classes} onclick={on_winner_update}>{team}</button>
-            });
+            let onclick = ctx.link().callback(move |_| Msg::UpdateWinner { index });
+
+            winners[index] = html! {
+                <button class={classes} {onclick}>{ team }</button>
+            };
         }
 
-        // SAFETY: All items in `teams` are initialized.
-        let teams: [Html; 2] = unsafe { mem::transmute(teams) };
-        let winner_input: [Html; 2] = unsafe { mem::transmute(winner_input) };
-
-        let on_submit = link.callback(|_| Msg::Submit);
+        let on_submit = ctx.link().callback(|_| Msg::Submit);
 
         html! {
             <div class="flex-col2">
@@ -144,12 +93,12 @@ impl Component for BracketUpdate {
                         <th>{ "Team" }</th>
                         <th>{ "Score" }</th>
                     </tr>
-                    { for teams.into_iter() }
+                    { for entrants.into_iter() }
                 </table>
                 <div class="winner-input-box">
                     <h3 class="h-center">{ "Declare a winner (optional)"}</h3>
                     <div class="flex-center winner-input-wrapper">
-                        { for winner_input.into_iter() }
+                        { for winners.into_iter() }
                     </div>
                 </div>
                 <button class="button" type="submit" onclick={on_submit} disabled=false>{ "Submit" }</button>
