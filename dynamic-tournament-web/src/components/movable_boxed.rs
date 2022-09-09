830e56e1c731f955129b9fa8437333038d390a64
MrGunflame <mrgunflame@protonmail.com>
2022-09-09 20:43:59 +0200

    Prevent stop of the drag when moving outside of the box

--- dynamic-tournament-web/src/components/movable_boxed.rs
+++ dynamic-tournament-web/src/components/movable_boxed.rs
@@ -1,24 +1,34 @@
-use yew::prelude::*;
+use std::cell::UnsafeCell;
 
 use gloo_events::{EventListener, EventListenerOptions};
 use wasm_bindgen::JsCast;
 use web_sys::{HtmlElement, MouseEvent, TouchEvent};
+use yew::prelude::*;
 
-use crate::components::button::Button;
+use crate::{
+    components::button::Button,
+    utils::{document, Rc},
+};
 
 // Zoom factor for mouse/touch scroll events.
 const ZOOM_FACTOR: f32 = 0.05;
 
 pub struct MovableBoxed {
+    /// NodeRef to the current movable-boxed element.
     element: NodeRef,
-    listeners: Option<[EventListener; 3]>,
+    /// The <body> element.
+    body: HtmlElement,
+
+    mouse_listeners: Option<[EventListener; 3]>,
+    touch_listeners: Option<[EventListener; 3]>,
 
     translate: Coordinates,
     last_move: Coordinates,
-    is_mouse_down: bool,
     scale: u32,
 
-    is_locked: bool,
+    /// Whether the box is currently allowed to be moved.
+    is_moving: Rc<UnsafeCell<bool>>,
+    is_locked: Rc<UnsafeCell<bool>>,
 }
 
 impl Component for MovableBoxed {
@@ -28,14 +38,17 @@ impl Component for MovableBoxed {
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
             element: NodeRef::default(),
-            listeners: None,
+            body: document().body().unwrap(),
+
+            mouse_listeners: None,
+            touch_listeners: None,
 
             translate: Coordinates::default(),
             last_move: Coordinates::default(),
-            is_mouse_down: false,
             scale: 100,
 
-            is_locked: false,
+            is_moving: Rc::new(UnsafeCell::new(false)),
+            is_locked: Rc::new(UnsafeCell::new(false)),
         }
     }
 
@@ -61,12 +74,28 @@ impl Component for MovableBoxed {
             }
             Message::MouseDown(coords) => {
                 self.last_move = coords;
-                self.is_mouse_down = true;
+
+                // SAFETY: Since events are executed synchronously on the main thread
+                // no other references exist.
+                unsafe {
+                    let is_moving = &mut *self.is_moving.get();
+                    *is_moving = true;
+                }
             }
-            Message::MouseUp => self.is_mouse_down = false,
+            Message::MouseUp => {
+                // SAFETY: Since events are executed synchronously on the main thread
+                // no other references exist.
+                unsafe {
+                    let is_moving = &mut *self.is_moving.get();
+                    *is_moving = false;
+                }
+            }
             Message::ZoomIn(amount) => self.scale = self.scale.saturating_add(amount),
             Message::ZoomOut(amount) => self.scale = self.scale.saturating_sub(amount),
-            Message::ToggleLock => self.is_locked = !self.is_locked,
+            Message::ToggleLock => unsafe {
+                let is_locked = &mut *self.is_locked.get();
+                *is_locked = !*is_locked;
+            },
         }
 
         true
@@ -77,11 +106,62 @@ impl Component for MovableBoxed {
             return;
         }
 
-        if let Some(element) = self.element.cast::<HtmlElement>() {
-            let is_locked = self.is_locked;
+        let options = EventListenerOptions::enable_prevent_default();
 
+        let is_moving = self.is_moving.clone();
+        let is_locked = self.is_locked.clone();
+
+        let onmousedown = ctx.link().batch_callback(move |event: MouseEvent| {
+            if unsafe { *is_locked.get() } {
+                return None;
+            }
+
+            event.prevent_default();
+
+            Some(Message::MouseDown(Coordinates {
+                x: event.client_x(),
+                y: event.client_y(),
+            }))
+        });
+
+        let onmousemove = ctx.link().batch_callback(move |event: MouseEvent| {
+            let is_moving = unsafe { *is_moving.get() };
+
+            if is_moving {
+                event.prevent_default();
+
+                Some(Message::Move(Coordinates {
+                    x: event.client_x(),
+                    y: event.client_y(),
+                }))
+            } else {
+                None
+            }
+        });
+
+        let onmouseup = ctx.link().callback(move |_: MouseEvent| Message::MouseUp);
+
+        let element = self.element.cast::<HtmlElement>().unwrap();
+        let mousedown =
+            EventListener::new_with_options(&element, "mousedown", options, move |event| {
+                onmousedown.emit(event.dyn_ref::<MouseEvent>().unwrap().clone());
+            });
+
+        let mousemove =
+            EventListener::new_with_options(&self.body, "mousemove", options, move |event| {
+                onmousemove.emit(event.dyn_ref::<MouseEvent>().unwrap().clone());
+            });
+
+        let mouseup = EventListener::new(&self.body, "mouseup", move |event| {
+            onmouseup.emit(event.dyn_ref::<MouseEvent>().unwrap().clone());
+        });
+
+        self.mouse_listeners = Some([mousedown, mousemove, mouseup]);
+
+        if let Some(element) = self.element.cast::<HtmlElement>() {
+            let is_locked = self.is_locked.clone();
             let ontouchstart = ctx.link().batch_callback(move |event: TouchEvent| {
-                if is_locked {
+                if unsafe { *is_locked.get() } {
                     return None;
                 }
 
@@ -93,8 +173,9 @@ impl Component for MovableBoxed {
                 }))
             });
 
+            let is_locked = self.is_locked.clone();
             let ontouchmove = ctx.link().batch_callback(move |event: TouchEvent| {
-                if is_locked {
+                if unsafe { *is_locked.get() } {
                     return None;
                 }
 
@@ -110,8 +191,6 @@ impl Component for MovableBoxed {
 
             let ontouchend = ctx.link().callback(move |_: ()| Message::MouseUp);
 
-            let options = EventListenerOptions::enable_prevent_default();
-
             let touchstart =
                 EventListener::new_with_options(&element, "touchstart", options, move |event| {
                     ontouchstart.emit(event.dyn_ref::<TouchEvent>().unwrap().clone());
@@ -126,7 +205,7 @@ impl Component for MovableBoxed {
                 ontouchend.emit(());
             });
 
-            self.listeners = Some([touchstart, touchmove, touchend]);
+            self.touch_listeners = Some([touchstart, touchmove, touchend]);
         }
     }
 
@@ -136,43 +215,10 @@ impl Component for MovableBoxed {
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        let is_locked = self.is_locked;
-
-        let on_mouse_up = ctx.link().callback(|_: MouseEvent| Message::MouseUp);
-
-        let on_mouse_down = ctx.link().batch_callback(move |e: MouseEvent| {
-            if is_locked {
-                return None;
-            }
-
-            e.prevent_default();
-
-            Some(Message::MouseDown(Coordinates {
-                x: e.client_x(),
-                y: e.client_y(),
-            }))
-        });
-
-        let is_mouse_down = self.is_mouse_down;
-        let on_mouse_move = ctx.link().batch_callback(move |e: MouseEvent| {
-            if is_locked {
-                return None;
-            }
-
-            if is_mouse_down {
-                e.prevent_default();
-
-                Some(Message::Move(Coordinates {
-                    x: e.client_x(),
-                    y: e.client_y(),
-                }))
-            } else {
-                None
-            }
-        });
+        let is_locked = self.is_locked.clone();
 
         let on_wheel = ctx.link().batch_callback(move |e: WheelEvent| {
-            if is_locked {
+            if unsafe { *is_locked.get() } {
                 return None;
             }
 
@@ -192,9 +238,9 @@ impl Component for MovableBoxed {
 
         let on_lock = ctx.link().callback(|_| Message::ToggleLock);
 
-        let cursor = if self.is_locked {
+        let cursor = if unsafe { *self.is_locked.get() } {
             "cursor: unset;"
-        } else if self.is_mouse_down {
+        } else if unsafe { *self.is_moving.get() } {
             "cursor: grabbing;"
         } else {
             "cursor: grab;"
@@ -205,7 +251,7 @@ impl Component for MovableBoxed {
             self.translate.x, self.translate.y, self.scale
         );
 
-        let lock_button = if is_locked {
+        let lock_button = if unsafe { *self.is_locked.get() } {
             html! {
                 <button class="button" onclick={on_lock} title="Unlock">
                     <i aria-hidden="true" class="fa-solid fa-lock-open"></i>
@@ -227,7 +273,7 @@ impl Component for MovableBoxed {
         };
 
         html! {
-            <div ref={self.element.clone()} class={classes} onmousedown={on_mouse_down} onmouseup={on_mouse_up} onmousemove={on_mouse_move} onwheel={on_wheel} style={cursor}>
+            <div ref={self.element.clone()} class={classes} onwheel={on_wheel} style={cursor}>
                 <div class="movable-boxed-buttons">
                     <Button onclick={on_reposition} title="Reposition">
                         <i aria-hidden="true" class="fa-solid fa-compress"></i>
